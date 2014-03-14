0743fa95f5727660827666e45f1c628f34ffd04d
Nicolas <nulysse4@gmail.com>
2014-03-14 16:53:04 -0400

    Work on error page

--- config.py
+++ config.py
@@ -2,4 +2,10 @@ CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
 source_directory = r'C:\Users\Nicolas\Desktop'
-destination_directory = r'C:\Intel'
+destination_directory = 'csxcsd'# r'C:\Intel'
+
+source_mount_command = 'notepad'
+source_unmount_command = 'calc'
+
+destination_mount_command = 'calc'
+destination_unmount_command = 'notepad'

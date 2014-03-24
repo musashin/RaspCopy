0d5b219bcf4a744fe0282da8b583acde3fd47d21
Nicolas <nulysse4@gmail.com>
2014-03-23 20:03:54 -0400

    tracking mechanism getting in place

--- app/async_task.py
+++ app/async_task.py
@@ -5,7 +5,9 @@ __author__ = 'Nicolas'
 from threading import Thread
 
 
-my_background_jobs = []
+my_background_jobs = dict()
+
+
 
 class BackgroundJob(Thread):
 
@@ -16,10 +18,12 @@ class BackgroundJob(Thread):
         self.job = job
         self.args = args
         self.kwargs = kwargs
-        my_background_jobs.append(self.name)
+        self.status = dict()
+        #TODO do not authorise duplicates!!
+        my_background_jobs[self.name] = self.status
 
     def run(self):
-        self.job(*self.args , **self.kwargs)
+        self.job(self.status, *self.args , **self.kwargs)
 
 
 def async(f):
@@ -35,9 +39,12 @@ if __name__ == "__main__":
     import time
 
     @async
-    def print_time(init_count, delay):
+    def print_time(status, init_count, delay):
         counter = init_count
         while counter:
+
+            status['status'] = 'count is {}'.format(str(counter))
+            status['percent'] = str((init_count-counter/init_count * 100.0))
 
             time.sleep(delay)
             print time.ctime(time.time())
@@ -49,4 +56,5 @@ if __name__ == "__main__":
     #job.start()
 
     while True:
-        pass
+        time.sleep(0.5)
+        print my_background_jobs['print_time']['status']
